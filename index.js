1170183d06d538d2b8587826dc428995c47ec60c
Craig Ford <craig_ford@icloud.com>
2022-01-31 12:21:57 -0500

    commit tree dcb03dd3e46e968bd0f67270a63877c62df98868

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+let companyName = 'Scuber';
+const mostProfitableNeighborhood = 'Chelsea';
+const companyCeo = 'Susan Smith';
+
+function myFunc() {
+    const companyName = 'Scuber';
+    let mostProfitableNeighborhood = 'Chelsea';
+    let companyCeo = 'Susan Smith';
+}
+
+
+
